efab65c519729d55b2a0e86fef312c03a219b6b3
Makar <zetikspb@yandex.ru>
2023-10-15 13:30:41 +0300

    Задача 1

---
+++ Zagotovka DZ3/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+if (num1 >= 100 && num1 < 1000)
+{
+    int result = (num1 % 100) / 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!");
+}
