eb32a7af2c920e81272b3c6d194bebb40e2c598a
Makar <zetikspb@yandex.ru>
2023-10-15 13:33:45 +0300

    Задача 3

---
+++ Zagotovka DZ3/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 == 1)
+{
+    Console.WriteLine("Нет");
+}
+if (num1 == 2)
+{
+    Console.WriteLine("Нет");
+}
+if (num1 == 3)
+{
+    Console.WriteLine("Нет");
+}
+if (num1 == 4)
+{
+    Console.WriteLine("Нет");
+}
+if (num1 == 5)
+{
+    Console.WriteLine("Нет");
+}
+if (num1 == 6)
+{
+    Console.WriteLine("Да");
+}
+if (num1 == 7)
+{
+    Console.WriteLine("Да");
+}
+else if (num1 < 1 || num1 > 7)
+{
+    Console.WriteLine("Введите число в заданном диапазоне!");
+}
